55923f09fd3fe351c1a1d3dd9a99a0aef2320cf2
XuGezheng <gezheng.xu@student.ecp.fr>
2018-11-05 20:07:52 +0100

    customer and GPS

---
+++ src/firstPart/Customer.java
@@ -0,0 +1,94 @@
+package firstPart;
+
+public class Customer {
+	
+	private static int counter = 0;
+	private String name;
+	private String surName;
+	private int numN;
+	private String idNum;
+	private int rideNum = 0;
+	private double onCarTime = 0;
+	private double onCarMoney = 0;
+	private GPSLocation gpsStart;
+	
+	public Customer(String name, String surName){
+		counter++;
+		numN = counter;
+		this.name = name;
+		this.surName = surName;
+		this.idNum = "customer"+numN;
+	}
+
+	
+	/* whaterver */
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSurName() {
+		return surName;
+	}
+
+	public void setSurName(String surName) {
+		this.surName = surName;
+	}
+
+	public int getNumN() {
+		return numN;
+	}
+
+	public void setNumN(int numN) {
+		this.numN = numN;
+	}
+
+	public String getIdNum() {
+		return idNum;
+	}
+
+	public void setIdNum(String idNum) {
+		this.idNum = idNum;
+	}
+
+	public int getRideNum() {
+		return rideNum;
+	}
+
+	public void setRideNum(int rideNum) {
+		this.rideNum = rideNum;
+	}
+
+	public double getOnCarTime() {
+		return onCarTime;
+	}
+
+	public void setOnCarTime(double onCarTime) {
+		this.onCarTime = onCarTime;
+	}
+
+	public double getOnCarMoney() {
+		return onCarMoney;
+	}
+
+	public void setOnCarMoney(double onCarMoney) {
+		this.onCarMoney = onCarMoney;
+	}
+
+	public GPSLocation getGpsStart() {
+		return gpsStart;
+	}
+
+	public void setGpsStart(GPSLocation gpsStart) {
+		this.gpsStart = gpsStart;
+	}
+	
+	/* get the amount of the customers registered*/
+	public int getCounter() {
+		return counter;
+	}
+	
+}
---
+++ src/firstPart/GPSLocation.java
@@ -0,0 +1,33 @@
+package firstPart;
+
+public class GPSLocation {
+	private double latitude;
+	private double longitude;
+	
+	public GPSLocation(double longitude, double latitude) {
+		this.longitude = longitude;
+		this.latitude = latitude;
+	}
+	
+	public GPSLocation() {
+		
+	}
+ 
+	public double getLatitude() {
+		return latitude;
+	}
+ 
+	public void setLatitude(double latitude) {
+		this.latitude = latitude;
+	}
+ 
+	public double getLongitude() {
+		return longitude;
+	}
+ 
+	public void setLongitude(double longitude) {
+		this.longitude = longitude;
+	}
+}
+
+
