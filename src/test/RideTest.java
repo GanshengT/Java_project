efc857035da4a2659ab15d165da1e51921bf44e8
TANGansheng <gansheng.tan@stuedent.ecp.fr>
2018-11-07 21:23:41 +0100

    refresh

---
+++ src/test/RideTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class RideTest {
+
+	@Test
+	public void testGetCustomers() {
+		fail("Not yet implemented");
+	}
+
+}
