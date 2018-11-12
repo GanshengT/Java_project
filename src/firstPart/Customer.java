5dac2469f79f235c77d8b1d9712cb104da063d76
XuGezheng <gezheng.xu@student.ecp.fr>
2018-11-12 23:06:52 +0100

    create a new ride

--- src/firstPart/Customer.java
+++ src/firstPart/Customer.java
@@ -85,12 +85,40 @@ public class Customer {
 	public Ride createANewRide(int passengerNumRequested, double desLongtude, double desLatitude, int startHH, int startMM) {
 		MyTime startTime = new MyTime(startHH, startMM, 0);
 		System.out.println("one request is sent");
-		Ride finalRide;
+		String rideType = "";
 		GPSLocation gpsEnd = new GPSLocation(desLongtude, desLatitude);
 		Ride rideX = new RideUberX(this, passengerNumRequested, this.gpsStart,gpsEnd, startTime);
 		Ride rideBlack = new RideUberX(this, passengerNumRequested, this.gpsStart,gpsEnd, startTime);
 		Ride rideVan = new RideUberVan(this, passengerNumRequested, this.gpsStart,gpsEnd, startTime);
 		Ride ridePool = new RideUberPool(this, passengerNumRequested, this.gpsStart,gpsEnd, startTime);
-		return rideX;
+		Ride rideError = new RideUberX(this,0,this.gpsStart, new GPSLocation(0,0),startTime);
+		if (passengerNumRequested>=5 & passengerNumRequested <= 6) {
+			System.out.printf("Your can choose a Van car for this ride, the price is: %g.\n"
+					+ "If you want to order, please input 'ubervan'.", rideVan.price());
+			rideType = "ubervan";
+		}else if(passengerNumRequested >= 1 & passengerNumRequested <=4) {
+			System.out.printf("An UberX price is: %geuros.\n"
+					+ "An UberBlack price is: %geuros.\n"
+					+ "An UberVan price is: %geuros.\n"
+					+ "An UberPool price is: %geuros.\n"
+					+ "Please input your choice.(\"uberx\" or \"uberblack\" or \"ubervan\" or \"uberpool\")\n", 
+					rideX.price(),rideBlack.price(),rideVan.price(),ridePool.price());
+			rideType = "uberx";
+		}else {
+			System.out.println("You have input a wrong passenger number.");
+		}
+	
+		if(rideType == "uberx") {
+			return rideX;
+		}else if(rideType == "uberblack") {
+			return rideBlack;
+		}else if(rideType == "ubervan") {
+			return rideVan;
+		}else if (rideType == "uberpool") {
+			return ridePool;
+		}else {
+			return rideError;
+		}
+		
 		}
 }
